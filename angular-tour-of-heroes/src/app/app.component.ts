f4fc56d05019a3115e29a775394c63911d2d8e6b
ROGER YAU <codes@y-unit.com>
2023-08-09 15:08:47 -0700

    Aug 9 2023

---
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular Tour of Heroes';
+  subtitle = 'My First Angular App';
+}
