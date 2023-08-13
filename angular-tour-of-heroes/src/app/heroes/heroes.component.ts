782d7fba36de79011cc78f2f2a3a6a19fa82e8ca
ROGER YAU <codes@y-unit.com>
2023-08-12 22:19:21 -0700

    4. Add services - Completed

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { Observable} from 'rxjs';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,11 +10,44 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 
-export class HeroesComponent {
-  heroes: Hero[] = HEROES;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
   selectedHero ?: Hero;
+
+  /* Auto wiring in shorthand constructor */
+  //constructor(private heroService: HeroService) {}
+
+  /* Auto wiring in longhand constructor */
+  // constructor(heroService : HeroService) {
+  //   this.heroService = heroService;
+  // }
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.populateHeroes();  // renaming it to populateHeroes() from getHeroes() for clarity
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   } 
+
+  // // keeping getHeroes() here is ok
+  // populateHeroes(): void {
+  //   // changed the syntax from anoynomous functions to named function 
+  //   // to hopefully make it easier to read
+  //   // but there is nothing wrong with what the instructions said
+  //   let obs: Observable <Hero[]> = this.heroService.getHeroes();
+  //   obs.subscribe(this.populateHeroesByParam); 
+  // }
+
+  populateHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  populateHeroesByParam(heroesToPopulate : Hero[]) : void {
+    this.heroes = heroesToPopulate;
+  }
 }
