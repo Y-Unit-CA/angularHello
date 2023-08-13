2799cba386c127ab966cc9ab46012b50b72515e2
ROGER YAU <codes@y-unit.com>
2023-08-13 16:17:15 -0700

    5. Add navigation - Completed

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Tour of Heroes';
-  subtitle = 'My First Angular App';
+  subtitle = 'Roger\'s Angular App';
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { Observable} from 'rxjs';
-import { MessageService } from '../message.service';
+// import { Observable} from 'rxjs';
+// import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,7 +12,7 @@ import { MessageService } from '../message.service';
 
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero ?: Hero;
+  // selectedHero ?: Hero;
 
   /* Auto wiring in shorthand constructor */
   //constructor(private heroService: HeroService) {}
@@ -22,16 +22,18 @@ export class HeroesComponent implements OnInit {
   //   this.heroService = heroService;
   // }
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  // constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.populateHeroes();  // renaming it to populateHeroes() from getHeroes() for clarity
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  } 
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  // } 
 
   // // keeping getHeroes() here is ok
   // populateHeroes(): void {
@@ -47,7 +49,7 @@ export class HeroesComponent implements OnInit {
         .subscribe(heroes => this.heroes = heroes);
   }
 
-  populateHeroesByParam(heroesToPopulate : Hero[]) : void {
-    this.heroes = heroesToPopulate;
-  }
+  // populateHeroesByParam(heroesToPopulate : Hero[]) : void {
+  //   this.heroes = heroesToPopulate;
+  // }
 }
